689c404f713f5b5e4f515cda5dc1a4e431b640b3
Shriomahlawat <ahlawatshriom48@gmail.com>
2025-11-02 15:13:39 +0530

    Create meter.py

---
+++ meter.py
@@ -0,0 +1,16 @@
+filename="energy_meter.csv"
+names=['voltage','current','power','class']
+dataset=read_csv(filename,names=names, skiprows=1)
+dataset[['voltage', 'current', 'power']] = dataset[['voltage', 'current', 'power']].apply(pd.to_numeric, errors='coerce')
+dataset.dropna(inplace=True)
+array=dataset.values
+x=array[:,0:3]
+y=array[:,3]
+x_train,x_validation,y_train,y_validation=train_test_split(x,y,test_size=0.2,random_state=1)
+model=SVC(gamma='auto')
+model.fit(x_train,y_train)
+result=model.score(x_validation,y_validation)
+print(result)
+value=[[212.7316,0.84753,180.296343]]
+prediction=model.predict(value)
+print(prediction[0])
