669be4ae8e03bc662f2c8e96a5bcb8a5d84cc891
Shriomahlawat <ahlawatshriom48@gmail.com>
2025-11-03 21:23:12 +0530

    Update meter.py

--- meter.py
+++ meter.py
@@ -1,21 +1,117 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # ENERGY METER USING ML
+
+# # Loading Dataset
+
+# In[ ]:
+
 
 from pandas import read_csv
-from sklearn.model_selection import train_test_split
+
+
+# In[ ]:
+
+
+url = "Energy Meter.csv"
+names = ['Voltage', 'Current', 'Power', 'class']
+dataset = read_csv(url, names=names)
+
+
+# # Summarize Dataset
+
+# In[ ]:
+
+
+print(dataset.shape)
+print(dataset.head(20))
+print(dataset.describe())
+print(dataset.groupby('class').size())
+
+
+# # Visualize Data
+
+# In[ ]:
+
+
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+
+
+# In[ ]:
+
+
+dataset.plot(kind='bar',subplots=True,layout=(2,2))
+pyplot.title('BAR PLOT')
+pyplot.show()
+
+dataset.hist()
+pyplot.title('HISTOGRAM PLOT')
+pyplot.show()
+
+scatter_matrix(dataset)
+pyplot.title('SCATTER PLOT')
+pyplot.show()
+
+
+# # Evaluating various ML Algorithm
+
+# In[ ]:
+
+
+# 6 ML Algorithm
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-import pandas as pd
-filename="energy_meter.csv"
-names=['voltage','current','power','class']
-dataset=read_csv(filename,names=names, skiprows=1)
-dataset[['voltage', 'current', 'power']] = dataset[['voltage', 'current', 'power']].apply(pd.to_numeric, errors='coerce')
-dataset.dropna(inplace=True)
-array=dataset.values
-x=array[:,0:3]
-y=array[:,3]
-x_train,x_validation,y_train,y_validation=train_test_split(x,y,test_size=0.2,random_state=1)
-model=SVC(gamma='auto')
-model.fit(x_train,y_train)
-result=model.score(x_validation,y_validation)
-print(result)
-value=[[212.7316,0.84753,180.296343]]
-prediction=model.predict(value)
-print(prediction[0])
+
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+
+
+# In[ ]:
+
+
+array = dataset.values
+X = array[:,0:3]
+y = array[:,3]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+
+
+# In[ ]:
+
+
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+
+
+# In[ ]:
+
+
+results = []
+names = []
+res = []
+for name, model in models:
+    kfold = StratifiedKFold(n_splits=10, random_state=None)
+    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+    results.append(cv_results)
+    names.append(name)
+    res.append(cv_results.mean())
+    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+pyplot.ylim(.990, .999)
+pyplot.bar(names, res, color ='maroon', width = 0.6)
+
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
