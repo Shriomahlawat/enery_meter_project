2c58f896369e3a5fe71f3d36c06400eebb5150bc
Shriomahlawat <ahlawatshriom48@gmail.com>
2025-11-03 21:32:10 +0530

    Update meter.py

--- meter.py
+++ meter.py
@@ -1,117 +1,91 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # ENERGY METER USING ML
-
-# # Loading Dataset
-
-# In[ ]:
-
-
+import streamlit as st
 from pandas import read_csv
-
-
-# In[ ]:
-
-
-url = "energy_meter1.csv"
-names = ['Voltage', 'Current', 'Power', 'class']
-dataset = read_csv(url, names=names)
-
-
-# # Summarize Dataset
-
-# In[ ]:
-
-
-print(dataset.shape)
-print(dataset.head(20))
-print(dataset.describe())
-print(dataset.groupby('class').size())
-
-
-# # Visualize Data
-
-# In[ ]:
-
-
 from pandas.plotting import scatter_matrix
 from matplotlib import pyplot
-
-
-# In[ ]:
-
-
-dataset.plot(kind='bar',subplots=True,layout=(2,2))
-pyplot.title('BAR PLOT')
-pyplot.show()
-
-dataset.hist()
-pyplot.title('HISTOGRAM PLOT')
-pyplot.show()
-
-scatter_matrix(dataset)
-pyplot.title('SCATTER PLOT')
-pyplot.show()
-
-
-# # Evaluating various ML Algorithm
-
-# In[ ]:
-
-
-# 6 ML Algorithm
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 
+# --- Streamlit App Title ---
+st.title("⚡ Energy Meter - Machine Learning Analysis")
 
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
+# --- Upload Dataset ---
+st.sidebar.header("Upload CSV File")
+uploaded_file = st.sidebar.file_uploader("Upload your Energy Meter CSV", type=["csv"])
 
+if uploaded_file is not None:
+    names = ['Voltage', 'Current', 'Power', 'class']
+    dataset = read_csv(uploaded_file, names=names)
 
-# In[ ]:
+    st.subheader("📊 Dataset Preview")
+    st.write(dataset.head(10))
 
+    st.subheader("Dataset Shape")
+    st.write(dataset.shape)
 
-array = dataset.values
-X = array[:,0:3]
-y = array[:,3]
-X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1, shuffle=True)
+    st.subheader("Class Distribution")
+    st.write(dataset.groupby('class').size())
 
+    st.subheader("Dataset Statistics")
+    st.write(dataset.describe())
 
-# In[ ]:
+    # --- Visualization Section ---
+    st.subheader("🔍 Data Visualization")
+    if st.checkbox("Show Bar Plot"):
+        dataset.plot(kind='bar', subplots=True, layout=(2, 2))
+        st.pyplot(pyplot)
 
+    if st.checkbox("Show Histogram"):
+        dataset.hist()
+        st.pyplot(pyplot)
 
-models = []
-models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-models.append(('LDA', LinearDiscriminantAnalysis()))
-models.append(('KNN', KNeighborsClassifier()))
-models.append(('CART', DecisionTreeClassifier()))
-models.append(('NB', GaussianNB()))
-models.append(('SVM', SVC(gamma='auto')))
+    if st.checkbox("Show Scatter Matrix"):
+        scatter_matrix(dataset)
+        st.pyplot(pyplot)
 
+    # --- Machine Learning Section ---
+    st.subheader("🤖 ML Model Comparison")
 
-# In[ ]:
+    array = dataset.values
+    X = array[:, 0:3]
+    y = array[:, 3]
+    X_train, X_validation, Y_train, Y_validation = train_test_split(
+        X, y, test_size=0.20, random_state=1, shuffle=True
+    )
 
+    models = [
+        ('LR', LogisticRegression(solver='liblinear', multi_class='ovr')),
+        ('LDA', LinearDiscriminantAnalysis()),
+        ('KNN', KNeighborsClassifier()),
+        ('CART', DecisionTreeClassifier()),
+        ('NB', GaussianNB()),
+        ('SVM', SVC(gamma='auto')),
+    ]
 
-results = []
-names = []
-res = []
-for name, model in models:
-    kfold = StratifiedKFold(n_splits=10, random_state=None)
-    cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
-    results.append(cv_results)
-    names.append(name)
-    res.append(cv_results.mean())
-    print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+    results = []
+    names = []
+    res = []
 
-pyplot.ylim(.990, .999)
-pyplot.bar(names, res, color ='maroon', width = 0.6)
+    for name, model in models:
+        kfold = StratifiedKFold(n_splits=10, random_state=None)
+        cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+        results.append(cv_results)
+        names.append(name)
+        res.append(cv_results.mean())
 
-pyplot.title('Algorithm Comparison')
-pyplot.show()
+    st.write("### Model Accuracy Results")
+    for i in range(len(names)):
+        st.write(f"{names[i]}: {res[i]:.4f}")
 
+    fig, ax = pyplot.subplots()
+    ax.bar(names, res, color='maroon', width=0.6)
+    ax.set_ylim(0.0, 1.0)
+    ax.set_title('Algorithm Comparison')
+    st.pyplot(fig)
+
+else:
+    st.info("👆 Please upload a CSV file to begin.")
